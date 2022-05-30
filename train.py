6b5c304118aea27d06458527f2b45e9305d6ac79
Yoonyoung (Jamie) Cho <jchocholate@gmail.com>
2022-05-30 21:53:32 +0900

    Improve train.py (#8)
    * auto configure runtime path and logging directory (`--path`)
    * fix typo in Conv2D documentation
    * suffix Conv3D/Conv2D with 3x3 for kernel size
    * save and load checkpoints (`--load_ckpt_file`)
    * logging to tqdm and tensorboard
    * add graph visualization to tensorboard

--- train.py
+++ train.py
@@ -4,13 +4,16 @@ from dataclasses import dataclass
 import itertools
 from simple_parsing import ArgumentParser
 from tqdm.auto import tqdm
-from typing import List, Optional, Tuple
+from os import PathLike
+from typing import List, Optional, Tuple, Union, Any, Dict
+from pathlib import Path
 
 import torch as th
 nn = th.nn
 F = nn.functional
 from data_loader import DSRDataset
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from model import DSRNet
 
 
@@ -29,6 +32,16 @@ class Config:
     loss_motion_weight: float = 1.0
     # Weight for mask loss
     loss_mask_weight: float = 5.0
+    load_ckpt_file: Optional[str] = None
+    path: str = '/tmp/dsr'
+
+
+def _ensure_dir(path: Union[str, PathLike]) -> Path:
+    """ensure that directory exists."""
+    path = Path(path).expanduser()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
 
 def mask_loss(
     logit: th.Tensor,
@@ -50,7 +63,7 @@ def mask_loss(
     ) -> th.Tensor:
         # CrossEntropyLoss on logits is equivalent to NLL on softmax result
         logit_permuted = th.stack(
-            [logit[b:b+1, -1]] + [logit[b:b+1, i] for i in permutation], dim=1)
+            [logit[b:b + 1, -1]] + [logit[b:b + 1, i] for i in permutation], dim=1)
         return F.cross_entropy(logit_permuted, mask_gt[b:b + 1])
     loss = 0
     B, K, S1, S2, S3 = logit.size()
@@ -72,10 +85,65 @@ def mask_loss(
             loss += best_loss
     return loss, best_mask_order
 
+
+def save_ckpt(ckpt_file: str, model: nn.Module,
+              optimizer: Optional[th.optim.Optimizer] = None):
+    ckpt_file = Path(ckpt_file)
+    _ensure_dir(ckpt_file.parent)
+    save_dict: Dict[str, Any] = {}
+    save_dict['model'] = model.state_dict()
+    if optimizer is not None:
+        save_dict['optimizer'] = optimizer.state_dict()
+    th.save(save_dict, str(ckpt_file))
+
+
+def load_ckpt(ckpt_file: str, model: nn.Module,
+              optimizer: Optional[th.optim.Optimizer] = None):
+    ckpt_file = Path(ckpt_file)
+    save_dict = th.load(str(ckpt_file))
+    model.load_state_dict(save_dict['model'])
+    if optimizer is not None:
+        optimizer.load_state_dict(save_dict['model'])
+
+
+def add_tensorboard_graph(model: nn.Module, loader: DataLoader,
+                          writer: SummaryWriter,
+                          device: th.device):
+    try:
+        train_mode = model.training
+        model.train(False)
+        data = iter(loader).next()[0]
+        keys = sorted(list(data.keys()))
+
+        # NOTE(ycho): We need to do this workaround
+        # because in torch < 1.10, the tracer
+        # fails to track `dict`-valued inputs and outputs.
+        class DummyModel(nn.Module):
+            def __init__(self, model: nn.Module):
+                super().__init__()
+                self.model = model
+
+            def forward(self, *args):
+                return tuple(self.model(
+                    {k: v.to(device) for k, v in zip(keys, args)}).values())
+        writer.add_graph(DummyModel(model),
+                         tuple(data[k].detach() for k in keys))
+        writer.flush()
+        # In case this op leaves any artifacts...
+        th.cuda.empty_cache()
+    finally:
+        model.train(train_mode)
+
+
 def main():
     parser = ArgumentParser()
     parser.add_arguments(Config, dest='cfg')
     cfg = parser.parse_args().cfg
+    root = Path(cfg.path)
+    index = len([d for d in root.glob('run-*') if d.is_dir()])
+    path = _ensure_dir(Path(cfg.path) / F'run-{index:03d}')
+    log_path = _ensure_dir(path / 'log')
+    print(F'Runtime path = {path}')
 
     device = th.device(cfg.device)
     dataset = DSRDataset(cfg.data_path, 'train', cfg.num_frames)
@@ -89,33 +157,54 @@ def main():
 
     optimizer = th.optim.Adam(model.parameters())
     motion_loss = nn.MSELoss()
+    writer = SummaryWriter(log_path)
 
+    # Add tensorboard graph visualization
+    # by converting inputs/outputs to a dummy model.
+    add_tensorboard_graph(model, loader, writer, device)
     loss_motion_weight = cfg.loss_motion_weight
     loss_mask_weight = cfg.loss_mask_weight
 
-    for epoch in tqdm(range(cfg.num_epoch), desc='epoch'):
-        # lr = adjust_learning_rate(...)
-        for data in tqdm(loader, leave=False, desc='batch'):
-            # Formatted as TxBx[...]
-            prv_state = None
-            mask_order = None
-            for i in range(cfg.num_frames):
-                inputs = data[i]
-                if prv_state is not None:
-                    inputs['prv_state'] = prv_state
-                outputs = model({k: v.to(device) for k, v in inputs.items()})
-                prv_state = outputs['state'].detach()
-                loss_motion = motion_loss(outputs['motion'],
-                                          inputs['scene_flow_3d'].to(device))
-                loss_mask, mask_order = mask_loss(
-                    outputs['logit'],
-                    inputs['mask_3d'].to(device),
-                    mask_order)
-                loss = loss_motion_weight * loss_motion + loss_mask_weight * loss_mask
-                print(F'loss = {loss.item()}')
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+    def _train_step(pbar, data, step):
+        # Formatted as TxBx[...]
+        prv_state = None
+        mask_order = None
+        for i in range(cfg.num_frames):
+            inputs = data[i]
+            if prv_state is not None:
+                inputs['prv_state'] = prv_state
+            outputs = model({k: v.to(device) for k, v in inputs.items()})
+            prv_state = outputs['state'].detach()
+            loss_motion = motion_loss(outputs['motion'],
+                                      inputs['scene_flow_3d'].to(device))
+            loss_mask, mask_order = mask_loss(
+                outputs['logit'],
+                inputs['mask_3d'].to(device),
+                mask_order)
+            loss = loss_motion_weight * loss_motion + loss_mask_weight * loss_mask
+            writer.add_scalar('loss', loss.item(), step + i)
+            pbar.set_postfix_str(F'loss={loss.item():03f}')
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        return step + cfg.num_frames
+
+    # Optionally load checkpoint.
+    if cfg.load_ckpt_file is not None:
+        load_ckpt(cfg.load_ckpt_file, model, optimizer)
+
+    # Train.
+    try:
+        step: int = 0
+        model.train(True)
+        for epoch in tqdm(range(cfg.num_epoch), desc='epoch'):
+            # lr = adjust_learning_rate(...)
+            with tqdm(loader, leave=False, desc='batch') as pbar:
+                for data in pbar:
+                    step = _train_step(pbar, data, step)
+            save_ckpt(F'{path}/dsr-{epoch:03d}.pt', model, optimizer)
+    finally:
+        save_ckpt(F'{path}/dsr-last.pt', model, optimizer)
 
 
 if __name__ == '__main__':
